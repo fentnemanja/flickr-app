45d481923cbb1d0772f563e7a562aa3a8e3da02c
Nemanja Fent <n.fent@vegaitsourcing.rs>
2018-10-26 14:44:56 +0200

    working on the markup and styling for the collections page

---
+++ flickr-app/src/components/Collection-banner.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <div class="collection-banner">
+            <div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1540345604302-828d409ed15a?ixlib=rb-0.3.5&s=2dab2365f26687e78075faccbf860909&auto=format&fit=crop&w=1950&q=80'"></div>
+            <div class="banner-fade"></div>
+            <h1>Gifted by the world’s most generous community of photographers</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Collection-banner',
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ flickr-app/src/components/Collection.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="collection-page">
+        <CollectionBanner/>
+        <ImageGallery/>
+    </div>
+</template>
+
+<script>
+import CollectionBanner from '@/components/Collection-banner';
+import ImageGallery from '@/components/Image-gallery';
+
+export default {
+    name: 'Collection',
+
+    components: {
+        CollectionBanner: CollectionBanner,
+        ImageGallery: ImageGallery
+    },
+
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
