93b737712e6bc6a11c0f35976221b516d5cba577
Nemanja Fent <fent.96n@gmail.com>
2018-12-02 18:39:13 +0100

    working recent photos on home page with lightbox

--- flickr-app/src/components/Image-gallery.vue
+++ flickr-app/src/components/Image-gallery.vue
@@ -9,60 +9,67 @@
             <!-- <p>http://farm{{post.farm}}.staticflickr.com/{{post.server}}/{{post.id}}_{{post.secret}}.jpg</p> -->
         <!-- </ul> -->
 
-        <ul>
+        <!-- <ul>
             <li v-for="(photo, index) in recentPhotos" :key="index">
 
                 <p>{{photo}}</p>
+                <br/>
             </li>
         </ul>
-
-        <a href="javascript:;" @click="loadMorePhotos(page++)">Load more photos</a>
+ -->
 
         <div class="image-gallery component-wrapper">
             <div class="gallery-grid">
                 <div class="image-tile" v-for="(photo, index) in recentPhotos" :key="index">
                     <div class="image-tile-header">
-                        <div class="author-image"></div>
+                        <div class="author-image"
+                            :style="{backgroundImage: 'url('+'http://farm'+photo.owner.iconfarm+'.staticflickr.com/'+photo.owner.iconserver+'/buddyicons/'+photo.owner.nsid+'.jpg'+')'}"></div>
                         <div class="image-tile-header__info">
-                            <span class="author-name">Nemanja Fent</span>
-                            <span class="image-date">April 05, 2018</span>
+                            <span class="author-name">{{photo.owner.username}}</span>
+                            <span class="image-date">{{photo.dates.taken}}</span>
                         </div>
                     </div>
-                    <a href="javascript:;" @click="openLightbox(photo.farm, photo.server, photo.id, photo.secret)">
+                    <a href="javascript:;" @click="openLightbox(photo)">
                         <div class="image-tile-img" :style="{backgroundImage: 'url('+'http://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'.jpg'+')'}">
                         </div>
                     </a>
                     <div class="image-tile-info">
                         <div class="image-title">
-                            <h4>{{photo.title}}</h4>
+                            <h4>{{photo.title._content}}</h4>
                         </div>
                         <div class="image-stats">
                             <ul>
                                 <li><i class="font-icon-heart"></i><span>46</span></li>
-                                <li><i class="font-icon-comment"></i><span>12</span></li>
-                                <li><i class="font-icon-eye"></i><span>134k</span></li>
+                                <li><i class="font-icon-comment"></i><span>{{photo.comments._content}}</span></li>
+                                <li><i class="font-icon-eye"></i><span>{{photo.views}}</span></li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="load-more">
+                <a href="javascript:;" class="round-btn green" @click="loadMorePhotos(page++)">Load more photos</a>
+            </div>
+
         </div>
 
-        <div class="shot-overlay js-shot-overlay" :class="{hidden: isShotLightboxOpen}">
-            <a @click="openLightbox" href="javascript:;" class="close-shot-lightbox">
+        <div v-if="displayLightbox" class="shot-overlay js-shot-overlay" :class="{hidden: isShotLightboxOpen}">
+            <a @click="closeLightbox" href="javascript:;" class="close-shot-lightbox">
                 <span class="font-icon-x"></span>
             </a>
             <div class="overlay-content">
                 <div class="shot-header">
                     <div class="shot-info-wrapper">
                         <a href="javascript:;">
-                            <div class="author-image"></div>
+                            <div class="author-image"
+                                :style="{backgroundImage: 'url('+'http://farm'+this.lightboxPhoto.owner.iconfarm+'.staticflickr.com/'+this.lightboxPhoto.owner.iconserver+'/buddyicons/'+this.lightboxPhoto.owner.nsid+'.jpg'+')'}"></div>
                         </a>
                         <div class="shot-header-info">
-                            <h3 class="shot-title">Simpler Times</h3>
+                            <h3 class="shot-title">{{this.lightboxPhoto.title._content}}</h3>
                             <div class="shot-info">
-                                <p>by <a class="image-author" href="javascript:;">Nemanja Fent</a></p>
-                                <p>on <span class="image-date">10 September, 2018</span></p>
+                                <p>by <a class="image-author" href="javascript:;">{{this.lightboxPhoto.owner.username}}</a></p>
+                                <p>on <span class="image-date">{{this.lightboxPhoto.dates.taken}}</span></p>
                             </div>
                         </div>
                         <div class="shot-header-cta">
@@ -80,35 +87,30 @@
                 </div>
                 <div class="shot-footer-wrapper">
                     <div class="shot-comment-section">
-                        <p class="photo-description">Working on a new identity for Swedish metal workshop</p>
+                        <h3>Description</h3>
+                        <p v-html="lightboxPhoto.description._content" class="photo-description"></p>
 
                         <div class="comments-wrapper">
-                            <h3>Responses</h3>
+                            <h3>Comments</h3>
 
                             <ul>
-                                <li class="comment">
-                                    <div class="user-image"></div>
+                                <li class="comment" v-for="(comment, index) in photoComments" :key="index">
                                     <div class="user-info">
-                                        <a href="javascript:;" class="username">Joe Dawson</a>
-                                        <p class="user-comment">Loving the way you illustrated the motion lines! But, I'm also curious about the Teletubbies look ;)</p>
-                                        <span class="comment-date">18 September, 2018</span>
+                                        <div class="user-image"
+                                        :style="{backgroundImage: 'url('+'http://farm'+comment.iconfarm+'.staticflickr.com/'+comment.iconserver+'/buddyicons/'+comment.author+'.jpg'+')'}"></div>
+                                        <a href="javascript:;" class="username">{{comment.authorname}}</a>
                                     </div>
-                                </li>
-
-                                <li class="comment">
-                                    <div class="user-image"></div>
-                                    <div class="user-info">
-                                        <a href="javascript:;" class="username">Joe Dawson</a>
-                                        <p class="user-comment">Loving the way you illustrated the motion lines! But, I'm also curious about the Teletubbies look ;)</p>
-                                        <span class="comment-date">18 September, 2018</span>
+                                    <div class="comment-info">
+                                        <p v-html="comment._content" class="user-comment"></p>
+                                        <!-- <span class="comment-date">18 September, 2018</span> -->
                                     </div>
                                 </li>
                             </ul>
-                            <div class="form-element">
+                            <!-- <div class="form-element">
                                 <h3>Add new comment</h3>
                                 <textarea class="comment-textbox" name="imagecomment"></textarea>
                                 <a class="btn" href="javascript:;">Post comment</a>
-                            </div>
+                            </div> -->
                         </div>
                     </div>
                     <div class="shot-information-section">
@@ -118,11 +120,7 @@
                                     <span class="font-icon-tag"></span>
                                 </div>
                                 <div class="shot-info">
-                                    <a class="btn btn-tag" href="javascript:;">branding</a>
-                                    <a class="btn btn-tag" href="javascript:;">logo</a>
-                                    <a class="btn btn-tag" href="javascript:;">nature</a>
-                                    <a class="btn btn-tag" href="javascript:;">dog</a>
-                                    <a class="btn btn-tag" href="javascript:;">hobby</a>
+                                    <a class="btn btn-tag" href="javascript:;" v-for="(tag, index) in this.lightboxPhoto.tags.tag" :key="index">{{tag.raw}}</a>
                                 </div>
                             </li>
 
@@ -131,7 +129,7 @@
                                     <span class="font-icon-eye"></span>
                                 </div>
                                 <div class="shot-info">
-                                    <span>1,893 views</span>
+                                    <span>{{this.lightboxPhoto.views}}</span>
                                 </div>
                             </li>
 
@@ -140,7 +138,7 @@
                                     <span class="font-icon-heart"></span>
                                 </div>
                                 <div class="shot-info">
-                                    <span>137 likes</span>
+                                    <span>{{this.likes}}</span>
                                 </div>
                             </li>
                         </ul>
@@ -148,6 +146,7 @@
                 </div>
             </div>
         </div>
+
     </section>
 </template>
 
@@ -162,15 +161,53 @@ export default {
         return {
             isShotLightboxOpen: true,
             posts: null,
-            recentPhotos: null,
-            lightboxPhoto: [],
+            recentPhotos: [],
+            lightboxPhoto: {
+                description: {
+                    _content: ''
+                },
+                title: {
+                    _content: ''
+                },
+                owner: {
+                    username: ''
+                },
+                views: '',
+                dates: {
+                    taken: ''
+                },
+                tags: {
+                    tag:[{id:'asda'}, {raw:'iff'}]
+                }
+            },
             page: 1,
+            recentUsernameRaw: [],
+            photoInfo: [],
+            likes: null,
+            photoComments: null,
+            displayLightbox: false,
         }
     },
 
     methods: {
-        openLightbox(farm, server, id, secret) {
-            this.lightboxPhoto = {farm:farm, server:server, id:id, secret:secret};
+        openLightbox(photo) {
+            this.displayLightbox = true;
+            this.lightboxPhoto = photo;
+
+            FlickrApi.getPhotoFavorites(photo.id).then(response => {
+                this.likes = response.photo.total;
+            })
+
+            FlickrApi.getPhotoComments(46120070001).then(response => {
+                var comments = response.comments.comment;
+
+                if(comments == true) {
+                    this.photoComments = comments.slice(0, 5);
+                } else {
+                    console.log('nema komentara');
+                    this.photoComments = [];
+                }
+            });
             
             this.isShotLightboxOpen = !this.isShotLightboxOpen;
             if(document.body.classList.contains('overflow-hidden')) {
@@ -180,24 +217,24 @@ export default {
             }
         },
 
+        closeLightbox() {
+            this.isShotLightboxOpen = !this.isShotLightboxOpen;
+            if(document.body.classList.contains('overflow-hidden')) {
+                document.body.classList.remove('overflow-hidden');
+            } else {
+                document.body.classList.add('overflow-hidden');
+            }
+        },
+
         loadMorePhotos(page) {
-            // console.log(this.page);
-
-            // console.log(typeof[this.posts]);
-
-            // console.log(this.posts);
-
             FlickrApi.getRecentPhotos(this.page, 10).then(response => {
                 var photoArray = response.photos.photo;
                 photoArray.forEach(element => {
-                    this.recentPhotos.push(element);
+                    FlickrApi.getPhotoInfo(element.id).then(response => {
+                    this.recentPhotos.push(response.photo);
+                    });
                 });
-
-                // this.test.push(posts.photos.photo);
-                console.log('posle update-a');
-                console.log(this.recentPhotos);
-            })
-            
+            });
         }
     },
 
@@ -205,8 +242,12 @@ export default {
         FlickrApi.getRecentPhotos(1, 10)
         .then(response => {
             this.posts = response
-        console.log(this.posts);
-            this.recentPhotos = response.photos.photo
+            var nemanja = response.photos.photo;
+            nemanja.forEach(element => {
+                FlickrApi.getPhotoInfo(element.id).then(response => {
+                this.recentPhotos.push(response.photo);
+                });
+            });
         });
     }
 }
--- flickr-app/src/services/api/flickr.js
+++ flickr-app/src/services/api/flickr.js
@@ -6,11 +6,72 @@ const perPage = 10;
 const page = 1;
 
 export default {
+
+    /*
+    * getRecent
+    * Returns a list of the latest public photos uploaded to flickr.
+    * Arguments:
+    *     -per_page (Number f photos to return per page)
+    *     -page (the page of results to return)
+    */
+
     getRecentPhotos(page, perPage) {
         var method = 'flickr.photos.getRecent';
         var url = baseUrl+method+`&`+`api_key=`+apiKey+`&`+`per_page=`+perPage+`&page=`+page+`&format=json&nojsoncallback=1`;
-        console.log(url);
         
+        return axios.get(url).then(response => {
+            return response.data
+        })
+    },
+
+    /*
+    * getInfo
+    * Get information about a photo. The calling user must have permission to view the photo.
+    * Arguments:
+    *    - photo_id (the id of the photo to get information for)
+    * 
+    */
+
+    getPhotoInfo(photoId) {
+        var method = 'flickr.photos.getInfo'
+        var url = baseUrl+method+`&`+`api_key=`+apiKey+`&`+`photo_id=`+photoId+`&format=json&nojsoncallback=1`;
+
+        return axios.get(url).then(response => {
+            return response.data
+        })
+    },
+
+    /*
+    * getFavorites
+    * Returns the list of people who have favorited a given photo.
+    * Arguments:
+    *    - photo_id (the id of the photo to get information for)
+    *    - per_page (number of usres to return per page)
+    * 
+    */
+
+    getPhotoFavorites(photoId) {
+        var method = 'flickr.photos.getFavorites'
+        var url = baseUrl+method+`&`+`api_key=`+apiKey+`&`+`photo_id=`+photoId+`&`+`per_page=`+1+`&format=json&nojsoncallback=1`;
+
+        return axios.get(url).then(response => {
+            return response.data
+        })
+    },
+
+
+    /*
+    * getComments
+    * Returns the comments for a photo.
+    * Arguments:
+    *    - photo_id (the id of the photo to get information for)
+    * 
+    */
+
+    getPhotoComments(photoId) {
+        var method = 'flickr.photos.comments.getList'
+        var url = baseUrl+method+`&`+`api_key=`+apiKey+`&`+`photo_id=`+photoId+`&format=json&nojsoncallback=1`;
+
         return axios.get(url).then(response => {
             return response.data
         })
